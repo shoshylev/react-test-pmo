4b509e3120fa93d97436c486d74d76fbdbe81365
Shoshi Rosental <srosental@planview.com>
2022-11-30 09:06:37 +0200

    form - get data from api - review

--- src/helpers/getUser.js
+++ src/helpers/getUser.js
@@ -3,11 +3,4 @@ import { useState } from "react";
 export const getUserData = async (userId) => {
     const res = await fetch(`https://jsonplaceholder.typicode.com/users/${userId || 1}`);
     return await res.json();
-    /*.then(res => res.json())
-    .then(result => {
-        console.log(result);
-        return result;
-    }
-    await return*/
-    //);
 }
